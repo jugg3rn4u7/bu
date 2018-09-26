95d20b87a381712b0d9871dff7b52daf8fa87966
Shrikant Kakaraparthi <shrikant.k@seventablets.com>
2018-09-25 21:18:11 -0500

    test attempt

--- src/android/Bu.java
+++ src/android/Bu.java
@@ -18,6 +18,14 @@ public class Bu extends CordovaPlugin {
 
     final private String TAG = "EADES BU PLUGIN";
     private static CallbackContext sCallbackContext;
+    private CordovaInterface cordova;
+
+    @Override 
+    public void initialize (CordovaInterface initCordova, CordovaWebView webView) {
+        Log.i(TAG, "initialize");
+        cordova = initCordova;
+        super.initialize (cordova, webView);
+    }
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
@@ -46,12 +54,50 @@ public class Bu extends CordovaPlugin {
             });
 
             // Assign blink up complete intent
-            blinkup.intentBlinkupComplete = new Intent(_context, BuResult.class);
+            //blinkup.intentBlinkupComplete = new Intent(_context, BuResult.class);
             
             return true;
         } 
         
         return false;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        blinkup.handleActivityResult(this, requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        blinkup.getTokenStatus(new TokenStatusCallback() {
+
+            @Override public void onSuccess(JSONObject json) {
+                Log.i(TAG, "TokenStatusCallback : SUCCESS : " + json.toString());
+                // return to callback
+                //Bu.getCallbackContext().success(json);
+
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, json.toString());
+                pluginResult.setKeepCallback(true);
+                Bu.getCallbackContext().sendPluginResult(pluginResult);
+            }
+
+            @Override public void onError(String errorMsg) {
+                Log.e(TAG, "TokenStatusCallback : ERROR : " + errorMsg);
+                Toast.makeText(_context, errorMsg, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override public void onTimeout() {
+                Log.e(TAG, "TokenStatusCallback : Timed out");
+                Toast.makeText(_context, "Timed out", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        blinkup.cancelTokenStatusPolling();
     }
 
     static CallbackContext getCallbackContext() {
